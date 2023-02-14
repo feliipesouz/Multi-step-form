4bd46d870578e981e98fca3fcb3299a279e93977
FELIPE ARRUDA DE SOUZA <felipe_rhnossa@3coracoes.com.br>
2023-02-14 13:47:22 -0300

    Step one integration with context

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import Theme from "../../components/Theme";
+import { FormActions, useForm } from "../../contexts/FormContext";
 import { Container } from "./styles";
 
 const FormStep1 = () => {
   const navigate = useNavigate();
+  const { state, dispatch } = useForm(); //Em state eu tenho os dados, e em dispatch eu consigo alterar os dados.
+
+  React.useEffect(() => {
+    dispatch({
+      type: FormActions.setLevel,
+      payload: 1,
+    });
+  }, []);
 
   const handleNextStep = () => {
-    navigate('/step2')
+    if (state.name !== "") {
+      navigate("/step2");
+    } else {
+      alert("Fill in the data");
+    }
+  };
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: FormActions.setName,
+      payload: e.target.value,
+    });
   };
 
   return (
     <Theme>
       <Container>
-        <p>Passo 1/3</p>
+        <p>Passo 1/3 - {state.level}</p>
         <h1>Vamos começar com seu nome</h1>
         <p>Preencha o campo abaixo com seu nome completo</p>
 
@@ -21,7 +41,7 @@ const FormStep1 = () => {
 
         <label>
           Seu nome completo
-          <input type={"text"} autoFocus />
+          <input type={"text"} autoFocus onChange={handleNameChange} />
         </label>
         <button onClick={handleNextStep}>Próximo</button>
       </Container>
